e990f5e51e28690e22b5c0c37b51f6a443ce2e54
Andrey Lebedev <alebedev424@gmail.com>
2025-07-03 13:29:05 +0300

    refactor: поправил работу ссылок

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { HiLink } from "react-icons/hi";
 import { MdArrowOutward } from "react-icons/md";
 import type { IProject } from "../../data/projects";
 import styles from "./ProjectCard.module.scss";
@@ -9,42 +11,61 @@ const ProjectCard = ({
   technologies,
   imgSrc,
   imgAlt,
-}: IProject) => (
-  <a
-    href={url}
-    target="_blank"
-    rel="noreferrer noopener"
-    aria-label={`${title} (opens in a new tab)`}
-    className={styles.cardLink}
-  >
-    <div className={styles.background}></div>
-    <img
-      src={imgSrc}
-      alt={imgAlt}
-      loading="lazy"
-      width={200}
-      height={48}
-      className={styles.image}
-    />
-    <div className={styles.content}>
-      <h3>
-        <span className={styles.titleLink}>
-          {title}{" "}
-          <MdArrowOutward className={styles.iconArrow} aria-hidden="true" />
-        </span>
-      </h3>
+}: IProject) => {
+  const [isLinkHovered, setIsLinkHovered] = useState(false);
+  return (
+    <a
+      href={url}
+      target="_blank"
+      rel="noreferrer noopener"
+      aria-label={`${title} (opens in a new tab)`}
+      className={`${styles.cardLink} ${
+        isLinkHovered ? styles.linkHovered : ""
+      }`}
+    >
+      <div className={styles.background}></div>
+      <img
+        src={imgSrc}
+        alt={imgAlt}
+        loading="lazy"
+        width={200}
+        height={48}
+        className={styles.image}
+      />
+      <div className={styles.content}>
+        <h3>
+          <span className={styles.title}>
+            {title}{" "}
+            <MdArrowOutward className={styles.iconArrow} aria-hidden="true" />
+          </span>
+        </h3>
 
-      <p className={styles.description}>{description}</p>
+        <p className={styles.description}>{description}</p>
 
-      <ul className={styles.techList} aria-label="Technologies used:">
-        {technologies.map((tech) => (
-          <li key={tech} className={styles.techItem}>
-            <div className={styles.techBadge}>{tech}</div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </a>
-);
+        <a
+          className={styles.link}
+          href={"#"}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={`${"#"} (откроется в новой вкладке)`}
+          onMouseEnter={() => setIsLinkHovered(true)}
+          onMouseLeave={() => setIsLinkHovered(false)}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <HiLink className={styles.icon} />
+          <span>{"label"}</span>
+        </a>
+
+        <ul className={styles.techList} aria-label="Technologies used:">
+          {technologies.map((tech) => (
+            <li key={tech} className={styles.techItem}>
+              <div className={styles.techBadge}>{tech}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </a>
+  );
+};
 
 export default ProjectCard;
