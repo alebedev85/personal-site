0f73bb116571c378c70386e6167529d9ec3de213
Andrey Lebedev <alebedev424@gmail.com>
2025-07-10 14:13:30 +0300

    fix: ошибка Contacts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import Hero from "../Hero/Hero";
 import NavLinks from "../NavLinks/NavLinks";
-import Contacts from "../Contacts/Contacts";
+import Contacts from "../Contacts1/Contacts";
 import styles from "./Header.module.scss";
 import clsx from "clsx";
 
